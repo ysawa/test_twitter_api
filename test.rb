c24e04927a3e861dc60a830e2ef771b68ab2fe32
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-02-23 19:01:19 +0900

    add: enable to test searching

--- test.rb
+++ test.rb
@@ -5,3 +5,8 @@
 $LOAD_PATH.push(File.dirname(__FILE__))
 
 require 'config/config'
+
+# search with keyword
+Twitter.search('ruby', lang: 'ja').statuses.each do |result|
+  puts "#{result.from_user}:#{result.text}"
+end
